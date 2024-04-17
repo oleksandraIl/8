2164ce5eb986ac45ff967abed779a114ee8b5015
oleksandraIl <144255710+oleksandraIl@users.noreply.github.com>
2024-04-17 14:16:50 +0300

    Update project.js

--- project.js
+++ project.js
@@ -1,1 +1,10 @@
-const fs=require('fs'),fileName='example.txt';fs.readFile(fileName,'utf8',(err,data)=>{if(err){console.error('Помилка під час зчитування файлу:',err);return;}console.log('Зчитаний текст з файлу:',data);});
+const fs = require('fs');
+const fileName = 'example.txt';
+
+fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Помилка під час зчитування файлу:', err);
+        return;
+    }
+    console.log('Зчитаний текст з файлу:', data);
+});
